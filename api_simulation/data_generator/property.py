3034f0f2f8962c631108d19395d4c3f7d5f3d062
ishami-i <i.irene@alustudent.com>
2026-07-13 07:34:50 +0200

    file for creating property information

---
+++ api_simulation/data_generator/property.py
@@ -0,0 +1,11 @@
+3. generate_properties()
+    Land Data
+        Unique Parcel Identifier (UPI)
+        Physical address
+        GIS coordinates (latitude and longitude)
+
+    Zoning and Restrictions
+        Land use classification
+        Permitted building rights
+        Environmental protection boundaries
+        Urban planning restrictions
