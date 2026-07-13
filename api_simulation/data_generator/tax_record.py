308aed3234f1cd2b1cd0c9c0da6f4e312c1d1f71
ishami-i <i.irene@alustudent.com>
2026-07-13 07:32:19 +0200

    file for tax info regsrding land for project

---
+++ api_simulation/data_generator/tax_record.py
@@ -0,0 +1,12 @@
+"""generate_tax_records()
+    Tax Status
+        Property tax clearance
+        Corporate tax compliance records
+        Outstanding tax obligations
+
+    Environmental and Business Clearances
+        Environmental Impact Assessment (EIA) approvals
+        Business licenses
+        Other regulatory approvals
+
+"""
