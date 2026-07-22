efc38c7452d9b0eaced0da3c2250a34eba9907aa
ishami-i <i.irene@alustudent.com>
2026-07-22 23:23:23 +0200

    inserted the default file in react

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vite.dev
 export default defineConfig({
   plugins: [
     react(),
