be4ec4f41f0e0b3fe5734e11f1adbf9469295b0f
ishami-i <i.irene@alustudent.com>
2026-07-25 00:19:12 +0200

    for alerts involving services

---
+++ frontend/src/services/alertService.js
@@ -0,0 +1,20 @@
+import api from "../api/axios";
+import { ALERT_API_URL, ALERT_API_ID_URL, ALERT_API_RESOLVE_URL } from "../config";
+
+export const getAlerts = async () => {
+  const response = await api.get(ALERT_API_URL);
+  return response.data;
+};
+
+export const getAlert = async (alertId) => {
+  const response = await api.get(ALERT_API_ID_URL.replace("{alert_id}", alertId));
+  return response.data;
+};
+
+export const resolveAlert = async (alertId, resolutionNote) => {
+  const response = await api.post(
+    ALERT_API_RESOLVE_URL.replace("{alert_id}", alertId),
+    { resolution_note: resolutionNote }
+  );
+  return response.data;
+};
