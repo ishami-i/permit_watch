0518d698ee4bd7bffb0ff9e141fe5d717693c4f0
ishami-i <i.irene@alustudent.com>
2026-07-24 11:57:30 +0200

    inserted the backend urls

---
+++ frontend/src/config.js
@@ -0,0 +1,9 @@
+export const API_BASE_URL = "http://localhost:8000/api";
+
+export const PERMIT_API_URL = `${API_BASE_URL}/permits/`;
+export const PERMIT_API_FULL_URL = `${API_BASE_URL}/permits/full/`;
+export const PERMIT_API_FULL_ID_URL = `${API_BASE_URL}/permits/full/{permit_id}/`;
+export const PERMIT_API_ID_URL = `${API_BASE_URL}/permits/{permit_id}/`;
+export const PERMIT_API_FLAGGED_URL = `${API_BASE_URL}/permits/flagged/`;
+export const PROJECT_API_FLAGGED_URL = `${API_BASE_URL}/projects/flagged/`;
+export const PERMIT_API_CHECK_URL = `${API_BASE_URL}/permits/{permit_id}/check/`;
