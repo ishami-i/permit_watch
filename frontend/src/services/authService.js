0564aeae8cfa92812b81d4f776b942a1e624620c
ishami-i <i.irene@alustudent.com>
2026-07-25 00:21:45 +0200

    authentication sevices

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,31 @@
+import api from "../api/axios";
+import { LOGIN_URL, LOGOUT_URL, FORGOT_PASSWORD_URL, CURRENT_USER_URL } from "../config";
+
+export const login = async (email, password) => {
+  const response = await api.post(LOGIN_URL, { email, password });
+  const { access, refresh, user } = response.data;
+
+  localStorage.setItem("access", access);
+  if (refresh) localStorage.setItem("refresh", refresh);
+
+  return user;
+};
+
+export const logout = async () => {
+  try {
+    await api.post(LOGOUT_URL);
+  } finally {
+    localStorage.removeItem("access");
+    localStorage.removeItem("refresh");
+  }
+};
+
+export const forgotPassword = async (email) => {
+  const response = await api.post(FORGOT_PASSWORD_URL, { email });
+  return response.data;
+};
+
+export const getCurrentUser = async () => {
+  const response = await api.get(CURRENT_USER_URL);
+  return response.data;
+};
