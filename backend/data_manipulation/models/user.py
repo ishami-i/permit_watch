d4b46c677c1dd2b5a63b849380fe7c24280d7ddd
ishami-i <i.irene@alustudent.com>
2026-07-24 11:53:35 +0200

    inserted auth user for authentication

--- backend/data_manipulation/models/user.py
+++ backend/data_manipulation/models/user.py
@@ -1,14 +1,21 @@
 from django.db import models
 from .role import Role
-
-
+from django.contrib.auth.models import AbstractUser
+from ...config import settings
 # create a user model
-class User(models.Model):
+class User(AbstractUser):
     class Status(models.TextChoices):
         ACTIVE = "active", "Active"
         INACTIVE = "inactive", "Inactive"
         BLOCKED = "blocked", "Blocked"
 
+    auth_user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="profile",
+        null=True,
+        blank=True,
+    )
     userId = models.AutoField(primary_key=True)
     user_name = models.CharField(max_length=250)
     user_email = models.EmailField(max_length=150, unique=True)
