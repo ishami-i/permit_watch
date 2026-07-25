e58f2b739b92f6b931718edd2f051b91f0120b29
ishami-i <i.irene@alustudent.com>
2026-07-26 00:08:25 +0200

    inserted the auth

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -52,6 +53,7 @@ REST_FRAMEWORK = {
 }
 AUTH_USER_MODEL = "data_manipulation.User"
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,3 +137,7 @@ PERMIT_API_URL = os.getenv(
     "PERMIT_API_URL",
     "http://127.0.0.1:5000/api/permits",
     )
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
