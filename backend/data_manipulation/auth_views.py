836b82cc470382e532b0c211495c8d398e255952
ishami-i <i.irene@alustudent.com>
2026-07-26 00:15:07 +0200

    created views for authentication

---
+++ backend/data_manipulation/auth_views.py
@@ -0,0 +1,20 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        user = self.user
+        data["user"] = {
+            "id": user.id,
+            "name": user.user_name,
+            "email": user.user_email,
+            "role": user.user_role.role_name if user.user_role else None,
+            "district": user.assigned_district.name if user.assigned_district else None,
+        }
+        return data
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
