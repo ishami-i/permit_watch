bd568c4a32d38e0da75ade56a3ca6305762766a4
ishami-i <i.irene@alustudent.com>
2026-07-27 00:19:41 +0200

    inserted the function to make sure it's ready

--- backend/data_manipulation/apps.py
+++ backend/data_manipulation/apps.py
@@ -1,5 +1,13 @@
+import os
+import sys
 from django.apps import AppConfig
 
 
 class DataManipulationConfig(AppConfig):
-    name = 'data_manipulation'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "data_manipulation"
+
+    def ready(self):
+        if "runserver" in sys.argv and os.environ.get("RUN_MAIN") == "true":
+            from .scheduler import start_scheduler
+            start_scheduler()
